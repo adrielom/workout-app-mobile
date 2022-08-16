01da9f4a114b4ecb46eda48848b450c8596bc70f
Adriel Oliveira <adriel.oliveira@dellead.com>
2022-08-16 16:09:47 -0300

    initialCommit(boot): Also added: * splash image * login bg image

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, NativeBaseProvider, Image } from 'native-base';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { SvgFromUri, SvgFromXml, SvgUri, SvgXml } from 'react-native-svg';
+import LogoFull from './src/components/LogoFull';
+import LoginScreen from './src/screens/Login';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <LoginScreen />
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
